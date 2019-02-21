191070db2f9d4ff053639b99310318afad12d4ee
JD <juandiegogarita@gmail.com>
2019-02-20 21:56:59 -0600

    enables the ability to receive emails

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
-	//require("class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
+	require("class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
 	
 	$firstname = 	$_POST['firstname'];
 	$lastname = 	$_POST['lastname'];
@@ -12,10 +12,10 @@ if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['emai
 	$mail->From = $email;
 	$mail->CharSet = "UTF-8";
 	$mail->FromName = $email;
-	$mail->addAddress("youremail@company.com"); 
+	$mail->addAddress("juandiegogarita@gmail.com"); 
 	
 	$mail->IsHTML(true);
-	$mail->Subject = $name." sent you an email";
+	$mail->Subject = $name."test";
 	
 	$mail->Body = $message."<br><br>".$name."<br>".$email."<br>".$phone."<br>";
 
