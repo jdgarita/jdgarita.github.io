128c772cad4bd18db48a43ab2e1227cda3839a18
JD <juandiegogarita@gmail.com>
2019-03-03 22:21:19 -0600

    misc updates

--- send.php
+++ send.php
@@ -1,16 +1,15 @@
 <?php
-if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
 	
 require 'PHPMailerAutoload.php';
 
-$firstname = 	$_POST['firstname'];
-$lastname = 	$_POST['lastname'];
+$name = 	$_POST['name'];
 $email = 		$_POST['email'];
 $message = 		$_POST['message'];
 
 
 $mail = new PHPMailer;
-$mail->setFrom($email, $firstname);
+$mail->setFrom($email, $name);
 $mail->addAddress('juandiegogarita@gmail.com', 'jd');
 $mail->Subject  = 'First PHPMailer Message';
 $mail->Body     = $message;
