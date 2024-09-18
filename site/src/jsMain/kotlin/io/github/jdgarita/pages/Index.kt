9bf51f054309a504ef361a9a5aaf175039d3b4da
JD <juandiegogarita@gmail.com>
2024-09-17 21:54:24 -0600

    updates home page structure

--- site/src/jsMain/kotlin/io/github/jdgarita/pages/Index.kt
+++ site/src/jsMain/kotlin/io/github/jdgarita/pages/Index.kt
@@ -8,22 +8,25 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
-import io.github.jdgarita.components.layouts.Main
+import io.github.jdgarita.components.sections.AboutSection
+import io.github.jdgarita.components.sections.ExperienceSection
+import io.github.jdgarita.components.sections.MainSection
 
 @Page
 @Composable
 fun HomePage() {
     var menuOpened by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Main(onMenuClicked = { menuOpened = true })
+            MainSection(onMenuClicked = { menuOpened = true })
+            AboutSection()
+            ExperienceSection()
         }
+
     }
 }
